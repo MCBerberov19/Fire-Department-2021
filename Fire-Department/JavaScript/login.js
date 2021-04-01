cf799fe8b0d36c0ba50eb6855e2ded272cecd78f
Mario Berberov <MCBerberov19@codingburgas.bg>
2021-04-01 19:36:30 +0300

    Create a hide and show for password button

--- Fire-Department/JavaScript/login.js
+++ Fire-Department/JavaScript/login.js
@@ -1,7 +1,24 @@
 function check() {
-    let username = document.getElementById("user").value;
-    if (username.length == 0) {
-        document.getElementById("user").style.borderColor = "red";
+
+    if (document.getElementById("user").value == "" || document.getElementById("pass").value == "") {
+        alert("Please fill the empty fields!");
+
+    } else {
+        alert("Welcome " + document.getElementById("user").value + " !");
+
 
     }
+}
+
+function hideAndShow() {
+    let view = document.getElementById("pass");
+    if (view.type === "password") {
+        view.type = "text";
+        document.getElementById("view").src = "../IMG/unview.png";
+    } else {
+        view.type = "password";
+        document.getElementById("view").src = "../IMG/view.png";
+    }
+
+
 }
