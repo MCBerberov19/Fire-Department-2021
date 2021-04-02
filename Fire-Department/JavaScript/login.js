f5bb79ea89641e88c98500ae6e24c59566f5ff0b
Mario Berberov <MCBerberov19@codingburgas.bg>
2021-04-02 09:15:33 +0300

    Create a functions when onclick out of the field color returns to red or green

--- Fire-Department/JavaScript/login.js
+++ Fire-Department/JavaScript/login.js
@@ -1,3 +1,6 @@
+var check1 = false;
+var check2 = false;
+
 function check() {
 
     let username = document.getElementById("user");
@@ -6,16 +9,38 @@ function check() {
         alert("Please fill the empty fields!");
         if (username.value == "") {
             username.style.borderColor = "#ff1a00";
+            check1 = true;
         }
 
         if (password.value == "") {
             password.style.borderColor = "#ff1a00";
+            check2 = true;
         }
     } else {
         window.open("../index.html", "_self", "replace")
         alert("Welcome " + username.value + " !");
     }
 
+}
+
+function onFocus(thing) {
+    thing.style.borderColor = "#1bbe31cc";
+}
+
+function onBlurUser() {
+    if (check1 == true) {
+        return document.getElementById("user").style.borderColor = "#ff1a00";
+    }
+
+    document.getElementById("user").style.borderColor = "#dad732cc";
+}
+
+function onBlurPass() {
+    if (check2 == true) {
+        return document.getElementById("pass").style.borderColor = "#ff1a00";
+    }
+
+    document.getElementById("pass").style.borderColor = "#dad732cc";
 }
 
 function hideAndShow() {
