62661249a13652e6f572ffd3cae700e2664b97f0
Mario Berberov <MCBerberov19@codingburgas.bg>
2021-04-04 17:12:51 +0300

    Add some style and section to the map page

--- Fire-Department/JavaScript/map.js
+++ Fire-Department/JavaScript/map.js
@@ -4,7 +4,13 @@ function initMap() {
         lng: 27.470030
     };
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
+        zoom: 11,
         center: location
+    });
+
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+
     });
 }
