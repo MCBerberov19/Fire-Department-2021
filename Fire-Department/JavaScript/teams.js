9f70de95f06306176934dfc7c4babb6023a0eb19
Mario Berberov <MCBerberov19@codingburgas.bg>
2021-04-18 12:05:33 +0300

    Add back-end when hover on name in teams page

---
+++ Fire-Department/JavaScript/teams.js
@@ -0,0 +1,168 @@
+var mike = document.getElementById("mikePopUp");
+var christian = document.getElementById("christianPopUp");
+var daniel = document.getElementById("danielPopUp");
+var james = document.getElementById("jamesPopUp");
+var jamie = document.getElementById("jamiePopUp");
+var ralph = document.getElementById("ralphPopUp");
+var alejandro = document.getElementById("alejandroPopUp");
+var jay = document.getElementById("jayPopUp");
+var manny = document.getElementById("mannyPopUp");
+var phil = document.getElementById("philPopUp");
+var aleksander = document.getElementById("aleksanderPopUp");
+var roberto = document.getElementById("robertoPopUp");
+
+function mikePopUpInfo() {
+
+    if (document.getElementById("mikePopUp").style.display = "none") {
+        document.getElementById("mikePopUp").style.display = "block";
+    }
+}
+
+function mikePopOutInfo() {
+    if (document.getElementById("mikePopUp").style.display = "block") {
+        document.getElementById("mikePopUp").style.display = "none";
+    }
+}
+
+function christianPopUpInfo() {
+
+    if (document.getElementById("christianPopUp").style.display = "none") {
+        document.getElementById("christianPopUp").style.display = "block";
+    }
+}
+
+function christianPopOutInfo() {
+    if (document.getElementById("christianPopUp").style.display = "block") {
+        document.getElementById("christianPopUp").style.display = "none";
+    }
+}
+
+function danielPopUpInfo() {
+
+    if (document.getElementById("danielPopUp").style.display = "none") {
+        document.getElementById("danielPopUp").style.display = "block";
+    }
+}
+
+function danielPopOutInfo() {
+    if (document.getElementById("danielPopUp").style.display = "block") {
+        document.getElementById("danielPopUp").style.display = "none";
+    }
+}
+
+function jamesPopUpInfo() {
+
+    if (document.getElementById("jamesPopUp").style.display = "none") {
+        document.getElementById("jamesPopUp").style.display = "block";
+    }
+}
+
+function jamesPopOutInfo() {
+    if (document.getElementById("jamesPopUp").style.display = "block") {
+        document.getElementById("jamesPopUp").style.display = "none";
+    }
+}
+
+function jamiePopUpInfo() {
+
+    if (document.getElementById("jamiePopUp").style.display = "none") {
+        document.getElementById("jamiePopUp").style.display = "block";
+    }
+}
+
+function jamiePopOutInfo() {
+    if (document.getElementById("jamiePopUp").style.display = "block") {
+        document.getElementById("jamiePopUp").style.display = "none";
+    }
+}
+
+function ralphPopUpInfo() {
+
+    if (document.getElementById("ralphPopUp").style.display = "none") {
+        document.getElementById("ralphPopUp").style.display = "block";
+    }
+}
+
+function ralphPopOutInfo() {
+    if (document.getElementById("ralphPopUp").style.display = "block") {
+        document.getElementById("ralphPopUp").style.display = "none";
+    }
+}
+
+function alejandroPopUpInfo() {
+
+    if (document.getElementById("alejandroPopUp").style.display = "none") {
+        document.getElementById("alejandroPopUp").style.display = "block";
+    }
+}
+
+function alejandroPopOutInfo() {
+    if (document.getElementById("alejandroPopUp").style.display = "block") {
+        document.getElementById("alejandroPopUp").style.display = "none";
+    }
+}
+
+function jayPopUpInfo() {
+
+    if (document.getElementById("jayPopUp").style.display = "none") {
+        document.getElementById("jayPopUp").style.display = "block";
+    }
+}
+
+function jayPopOutInfo() {
+    if (document.getElementById("jayPopUp").style.display = "block") {
+        document.getElementById("jayPopUp").style.display = "none";
+    }
+}
+
+function mannyPopUpInfo() {
+
+    if (document.getElementById("mannyPopUp").style.display = "none") {
+        document.getElementById("mannyPopUp").style.display = "block";
+    }
+}
+
+function mannyPopOutInfo() {
+    if (document.getElementById("mannyPopUp").style.display = "block") {
+        document.getElementById("mannyPopUp").style.display = "none";
+    }
+}
+
+function philPopUpInfo() {
+
+    if (document.getElementById("philPopUp").style.display = "none") {
+        document.getElementById("philPopUp").style.display = "block";
+    }
+}
+
+function philPopOutInfo() {
+    if (document.getElementById("philPopUp").style.display = "block") {
+        document.getElementById("philPopUp").style.display = "none";
+    }
+}
+
+function aleksanderPopUpInfo() {
+
+    if (document.getElementById("aleksanderPopUp").style.display = "none") {
+        document.getElementById("aleksanderPopUp").style.display = "block";
+    }
+}
+
+function aleksanderPopOutInfo() {
+    if (document.getElementById("aleksanderPopUp").style.display = "block") {
+        document.getElementById("aleksanderPopUp").style.display = "none";
+    }
+}
+
+function robertoPopUpInfo() {
+
+    if (document.getElementById("robertoPopUp").style.display = "none") {
+        document.getElementById("robertoPopUp").style.display = "block";
+    }
+}
+
+function robertoPopOutInfo() {
+    if (document.getElementById("robertoPopUp").style.display = "block") {
+        document.getElementById("robertoPopUp").style.display = "none";
+    }
+}
