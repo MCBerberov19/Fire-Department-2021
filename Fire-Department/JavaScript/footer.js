000ade810265367c3eac5e0ca22289592f27ccf1
Mario Berberov <MCBerberov19@codingburgas.bg>
2021-04-19 22:26:12 +0300

    Fix footer bug

--- Fire-Department/JavaScript/footer.js
+++ Fire-Department/JavaScript/footer.js
@@ -13,7 +13,7 @@ function openOutlook() {
 function openPlace() {
     let choice = confirm("Are you sure you want to continue?");
     if (choice == true) {
-        window.open("https://www.google.bg/maps/place/%D0%9F%D0%BE%D0%B6%D0%B0%D1%80%D0%BD%D0%B0+%D0%B8+%D0%B0%D0%B2%D0%B0%D1%80%D0%B8%D0%B9%D0%BD%D0%B0+%D0%B1%D0%B5%D0%B7%D0%BE%D0%BF%D0%B0%D1%81%D0%BD%D0%BE%D1%81%D1%82/@42.5632862,27.5111681,17z/data=!3m1!4b1!4m5!3m4!1s0x40a696b5456f591b:0x3254fd0d18c27b3a!8m2!3d42.5632823!4d27.5133568");
+        window.open("https://www.google.bg/maps/place/%D0%91%D1%83%D1%80%D0%B3%D0%B0%D1%81/@42.5266271,27.3819262,12z/data=!3m1!4b1!4m5!3m4!1s0x40a69266f9fc9d91:0x400a01269bf4df0!8m2!3d42.5047926!4d27.4626361");
     }
 }
 
