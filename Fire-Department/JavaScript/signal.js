6413ec3c0007c209a728d75b43d5fbfc39ea21fc
Mario Berberov <MCBerberov19@codingburgas.bg>
2021-04-22 12:14:46 +0300

    Add back-end to give a signal page

--- Fire-Department/JavaScript/signal.js
+++ Fire-Department/JavaScript/signal.js
@@ -15,7 +15,7 @@ function initMap() {
     });
 }
 
-function check() {
+/*function check() {
     let number = document.getElementById("number");
     if (isNaN(number.value) == true) {
         alert("Enter a valid number !");
@@ -23,4 +23,37 @@ function check() {
     }
 
 
+}*/
+
+function check() {
+    let x = document.getElementById("teams").style.borderColor;
+    if ((document.getElementById("number").value == "" || isNaN(document.getElementById("number").value) == true) || document.getElementById("teams").value == '' || document.getElementById("adress").value == "") {
+        document.getElementById("showText").innerHTML = "Please fill the blanks!*";
+        document.getElementById("showText").style.color = "#ff1a00";
+        if (isNaN(document.getElementById("number").value) == true) {
+            document.getElementById("number").value = "";
+            document.getElementById("number").style.borderColor = "#ff1a00";
+            alert("Enter a valid number!");
+        } else {
+            document.getElementById("number").style.borderColor = "rgb(255, 137, 26)";
+        }
+        if (document.getElementById("number").value == "") {
+            document.getElementById("number").style.borderColor = "#ff1a00";
+        } else {
+            document.getElementById("number").style.borderColor = "rgb(255, 137, 26)";
+        }
+        if (document.getElementById("adress").value == "") {
+            document.getElementById("adress").style.borderColor = "#ff1a00";
+        } else {
+            document.getElementById("adress").style.borderColor = "rgb(255, 137, 26)";
+        }
+        if (document.getElementById("teams").value == '') {
+            document.getElementById("teams").style.borderColor = "red";
+        } else {
+            document.getElementById("teams").style.borderColor = "black";
+        }
+    } else {
+        alert(`We sent to the adress team ${document.getElementById("teams").value} !`);
+        window.location.reload();
+    }
 }
